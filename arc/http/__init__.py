3a613e93586dfad0adfa35d4d1a64a538a1c264c
Luke Petre <lpetre@gmail.com>
2020-12-22 23:24:43 +0000

    Add support for ddb sessions in python  - structured identically to ddb sessions in nodejs  - should be completely compatible to nodejs ddb sessions

--- arc/http/__init__.py
+++ arc/http/__init__.py
@@ -1,24 +1,24 @@
 import os
 from .session.cookies import _write_cookie, _read_cookie
 from .session.jwe import jwe_read, jwe_write
+from .session.ddb import ddb_read, ddb_write
 
 COOKIE_NAME: str = "_idx"
+DEFAULT_SESSION_TABLE: str = "arc-sessions"
 
 
 def session_read(req):
-    if os.environ.get("SESSION_TABLE_NAME") == "jwe":
-        try:
-            cookie = _read_cookie(req, COOKIE_NAME)
-            return jwe_read(cookie)
-        except:
-            return {}
-
-    raise NotImplementedError()
+    table_name = os.environ.get("SESSION_TABLE_NAME", DEFAULT_SESSION_TABLE)
+    try:
+        cookie = _read_cookie(req, COOKIE_NAME)
+        return jwe_read(cookie) if table_name == "jwe" else ddb_read(cookie, table_name)
+    except:
+        return {}
 
 
 def session_write(payload):
-    if os.environ.get("SESSION_TABLE_NAME") == "jwe":
-        cookie = jwe_write(payload)
-        return _write_cookie(cookie, COOKIE_NAME)
-
-    raise NotImplementedError()
+    table_name = os.environ.get("SESSION_TABLE_NAME", DEFAULT_SESSION_TABLE)
+    cookie = (
+        jwe_write(payload) if table_name == "jwe" else ddb_write(payload, table_name)
+    )
+    return _write_cookie(cookie, COOKIE_NAME)
---
+++ arc/http/session/ddb.py
@@ -0,0 +1,66 @@
+import os
+from datetime import datetime, timedelta
+from base64 import b64encode, urlsafe_b64encode
+from typing import Any, Dict, Tuple, Optional
+from cryptography.hazmat.primitives import hashes, hmac
+from arc.tables import table
+
+
+def _get_secret() -> str:
+    return os.environ.get("ARC_APP_SECRET", os.environ.get("ARC_APP_NAME", "fallback"))
+
+
+def _sign(value: str, secret: str) -> str:
+    h = hmac.HMAC(secret.encode("utf-8"), hashes.SHA256())
+    h.update(value.encode("utf-8"))
+    digest = b64encode(h.finalize()).decode("utf-8").rstrip("=")
+    return value + "." + digest
+
+
+def _unsign(value: str, secret: str) -> Tuple[str, bool]:
+    where = value.rfind(".")
+    before, after = value[:where], value[where + 1 :]
+    return (before, _sign(before, secret) == value)
+
+
+def _week_from_now() -> int:
+    return int((datetime.now() + timedelta(weeks=1)).timestamp())
+
+
+def _uid_safe(byte_length: int) -> str:
+    buff = os.urandom(byte_length)
+    return urlsafe_b64encode(buff).decode("utf-8").rstrip("=")
+
+
+def ddb_read(cookie: Optional[str], table_name: str) -> Dict[Any, Any]:
+    db = table(table_name)
+
+    if cookie is not None:
+        secret = _get_secret()
+        cookie, valid = _unsign(cookie, secret)
+        if not valid:
+            cookie = None
+
+    # create a new session
+    if cookie is None:
+        session = {
+            "_idx": _uid_safe(18),
+            "_secret": _uid_safe(18),
+            "_ttl": _week_from_now(),
+        }
+        db.put_item(Item=session)
+        return session
+
+    session = db.get_item(Key={"_idx": cookie}, ConsistentRead=True)
+    return session.get("Item", {})
+
+
+def ddb_write(payload: Dict[Any, Any], table_name: str) -> str:
+    payload = dict(payload)
+    payload["_ttl"] = _week_from_now()
+
+    db = table(table_name)
+    db.put_item(Item=payload)
+
+    secret = _get_secret()
+    return _sign(payload.get("_idx"), secret)
