cf6dccdd164b6673da48264a5bc2c324fa10d770
Bård Bachmann <bardbachmann@gmail.com>
2021-03-11 10:48:42 +0100

    Find ssm parameters recursively

--- arc/__init__.py
+++ arc/__init__.py
@@ -4,12 +4,30 @@ from typing import Dict
 from collections import defaultdict
 
 
+def get_params_recursive(ssm, path, params=[], nextToken=None):
+    result = {}
+
+    if nextToken is None:
+        result = ssm.get_parameters_by_path(Path=path, Recursive=True)
+    else:
+        result = ssm.get_parameters_by_path(
+            Path=path, Recursive=True, NextToken=nextToken
+        )
+
+    if "NextToken" in result:
+        return get_params_recursive(
+            ssm, path, params + result["Parameters"], result["NextToken"]
+        )
+    else:
+        return params + result["Parameters"]
+
+
 def reflect() -> Dict[str, Dict[str, str]]:
     path = "/" + os.environ["ARC_CLOUDFORMATION"]
     ssm = boto3.client("ssm")
-    res = ssm.get_parameters_by_path(Path=path, Recursive=True)
+    res = get_params_recursive(ssm, path)
     result = defaultdict(dict)
-    for x in res["Parameters"]:
+    for x in res:
         key, val = x["Name"], x["Value"]
         bits = key.split("/")
         if len(bits) < 4:
