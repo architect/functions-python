1c51a0710abf80b28f7462387e1615827d1f3308
Ryan Block <rblock@gmail.com>
2023-08-14 11:48:48 -0700

    Generate PyPI-friendly readme file

---
+++ scripts/generate-pypi-readme.py
@@ -0,0 +1,13 @@
+# PyPI does not support light/dark mode header images yet, per: https://github.com/pypi/warehouse/issues/11251
+# So for now, we'll have to do it manually
+
+raw = open("./readme.md", encoding="utf-8")
+readme = raw.readlines()
+
+img_tag = '<img alt="Architect Logo" src="https://github.com/architect/assets.arc.codes/raw/main/public/architect-logo-500b%402x.png">'
+readme.insert(0, img_tag)
+del readme[1:4]
+
+new_readme = open("./readme_pypi.md", "w", encoding="utf-8")
+new_readme.write("".join(readme))
+print("Generated PyPI friendly readme")
